8dd77eebaaa9ecf3893a821eb0679106c51c0bc2
Udoh Lawrence <lawrenceaudoh@gmail.com>
2024-10-27 22:46:49 +0100

    FEATURE: add error/success messages to login

--- start/routes.ts
+++ start/routes.ts
@@ -4,6 +4,7 @@ const LoginController = () => import('#controllers/login_controller')
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
 router.get('/register', [RegisterController, 'show'])
 router.post('/register', [RegisterController, 'store'])
 
